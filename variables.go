a63ea1ec4d96c3b3a0bba4230d0148d5ec558f27
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-10-23 23:34:05 +0300

    fix import paths
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- variables.go
+++ variables.go
@@ -11,12 +11,12 @@ var (
 	gorillaMuxPackage      = protogen.GoImportPath("github.com/gorilla/mux")
 	chiPackage             = protogen.GoImportPath("github.com/go-chi/chi/v5")
 	chiMiddlewarePackage   = protogen.GoImportPath("github.com/go-chi/chi/v5/middleware")
-	microApiPackage        = protogen.GoImportPath("github.com/unistack-org/micro/v3/api")
-	microClientPackage     = protogen.GoImportPath("github.com/unistack-org/micro/v3/client")
-	microServerPackage     = protogen.GoImportPath("github.com/unistack-org/micro/v3/server")
-	microClientHttpPackage = protogen.GoImportPath("github.com/unistack-org/micro-client-http/v3")
-	microCodecPackage      = protogen.GoImportPath("github.com/unistack-org/micro/v3/codec")
+	microApiPackage        = protogen.GoImportPath("go.unistack.org/micro/v3/api")
+	microClientPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/client")
+	microServerPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/server")
+	microClientHttpPackage = protogen.GoImportPath("go.unistack.org/micro-client-http/v3")
+	microCodecPackage      = protogen.GoImportPath("go.unistack.org/micro/v3/codec")
 	timePackage            = protogen.GoImportPath("time")
 	deprecationComment     = "// Deprecated: Do not use."
-	versionComment         = "v3.4.2"
+	versionComment         = "v3.5.2"
 )
