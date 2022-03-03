41e32e36b33ff45462e4b1e5251508ef3ea29424
Vasiliy Tolstov <v.tolstov@unistack.org>
2022-03-03 19:11:57 +0300

    fix for empty default response
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- variables.go
+++ variables.go
@@ -16,8 +16,8 @@ var (
 	microServerPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/server")
 	microClientHttpPackage = protogen.GoImportPath("go.unistack.org/micro-client-http/v3")
 	microServerHttpPackage = protogen.GoImportPath("go.unistack.org/micro-server-http/v3")
-	microCodecPackage      = protogen.GoImportPath("go.unistack.org/micro-proto/v3/codec")
-	microErrorsPackage     = protogen.GoImportPath("go.unistack.org/micro-proto/v3/errors")
+	microCodecPackage      = protogen.GoImportPath("go.unistack.org/micro/v3/codec")
+	microErrorsPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/errors")
 	timePackage            = protogen.GoImportPath("time")
 	deprecationComment     = "// Deprecated: Do not use."
 	versionComment         = "v3.5.3"
