8e9be8cda7fda6d1aeb0be72d604bf2cae383924
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-05-11 10:36:39 +0300

    fixup rpc CloseSend method
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- variables.go
+++ variables.go
@@ -21,5 +21,5 @@ var (
 	grpcPackage            = protogen.GoImportPath("google.golang.org/grpc")
 	timePackage            = protogen.GoImportPath("time")
 	deprecationComment     = "// Deprecated: Do not use."
-	versionComment         = "v4.0.0"
+	versionComment         = "v4.0.1"
 )
