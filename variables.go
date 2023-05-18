489c58fae705dcdc6ba6f2b2a6bddb0d4b2b4cd1
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-05-19 00:29:23 +0300

    fix codec.Frame usage
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- variables.go
+++ variables.go
@@ -16,10 +16,10 @@ var (
 	microServerPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/server")
 	microClientHttpPackage = protogen.GoImportPath("go.unistack.org/micro-client-http/v3")
 	microServerHttpPackage = protogen.GoImportPath("go.unistack.org/micro-server-http/v3")
-	microCodecPackage      = protogen.GoImportPath("go.unistack.org/micro/v3/codec")
+	microCodecPackage      = protogen.GoImportPath("go.unistack.org/micro-proto/v3/codec")
 	microErrorsPackage     = protogen.GoImportPath("go.unistack.org/micro/v3/errors")
 	grpcPackage            = protogen.GoImportPath("google.golang.org/grpc")
 	timePackage            = protogen.GoImportPath("time")
 	deprecationComment     = "// Deprecated: Do not use."
-	versionComment         = "v3.10.3"
+	versionComment         = "v3.10.4"
 )
